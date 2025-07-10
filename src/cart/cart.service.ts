2db670587bfd71c50c7836dc8afdeeee8aa0aade
Bryant C Mejia <bryantcmejia@gmail.com>
2025-07-09 18:41:11 -0600

    Agregando dto y servicio de cart

---
+++ src/cart/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class CartService {
+  constructor(private prisma: PrismaService) {}
+
+  async getCart(userId: number) {
+    return this.prisma.cart.findUnique({
+      where: { userId },
+      include: { items: { include: { product: true } } },
+    });
+  }
+
+  async addProduct(userId: number, productId: number) {
+    
+  }
+
+  async removeProduct(userId: number, productId: number) {
+    
+  }
+}
